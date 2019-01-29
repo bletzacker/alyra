0bad5cea8ad5ade5abb2d08f396302910dc9a5dd
Laurent BLETZACKER <laurent@bletzacker.io>
2019-01-29 21:21:30 +0100

    Exercice 1.1.1 version 1

--- 1/1/1/1.1.1.py
+++ 1/1/1/1.1.1.py
@@ -1,1 +1,35 @@
+# -*- coding: utf-8 -*-
+import random
 
+target = random.randint(1, 100)
+notFound = True
+
+print("J'ai tiré aléatoirement un nombre entre 1 et 100 que je vous propose de retrouver.\n")
+
+while notFound:
+
+    notIntegerBetweenOneAndHundred = True
+    while notIntegerBetweenOneAndHundred :
+        proposal = input("Entrez votre proposition sous la forme d'un entier entre 1 et 100 : ")
+        if proposal.isdigit() :
+            proposal = int(proposal)
+            if 1 <= proposal <= 100 :
+                notIntegerBetweenOneAndHundred = False
+
+    difference = proposal - target
+
+    if difference > 0 :
+        print("Mon nombre est inférieur.")
+    else :
+        print("Mon nombre est supérieur.")
+
+    if abs(difference) <= 5 :
+        print("De plus, votre proposition est très proche, moins de 5 unités d'écart.\n")
+    elif abs(difference) <= 10 :
+        print("De plus, Votre proposition est proche, entre 6 et 10 unités d'écart.\n")
+    else :
+        print("De plus, votre proposition est éloignée de plus de 10 unités.\n")
+
+    if difference == 0 :
+        print("Bravo ! Vous avez trouvé le nombre que j'ai tiré aléatoirement.\n")
+        notFound = False
