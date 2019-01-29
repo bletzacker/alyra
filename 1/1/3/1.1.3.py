7cd80946626053942686810bf70c3ab225598d58
Laurent BLETZACKER <laurent@bletzacker.io>
2019-01-29 23:54:47 +0100

    Exercice 1.1.3 version 1

--- 1/1/3/1.1.3.py
+++ 1/1/3/1.1.3.py
@@ -1,1 +1,13 @@
+# -*- coding: utf-8 -*-
 
+def palindrome(string):
+    str = string
+    str = ''.join(str.split())
+    for i in range(int(len(string)/2)) :
+        if str[i] != str[len(str)-1-i] :
+            return print(string + " n'est pas un palindrome.")
+    return print(string + " est un palindrome.")
+
+palindrome("ressasser")
+palindrome("alyra")
+palindrome("ESOPE RESTE ICI ET SE REPOSE")
