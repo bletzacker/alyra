e7bb121ac29683ef08883335215070266159d539
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-02 17:14:01 +0100

    Exercice 1.3.2 version 1

--- 1/3/2/1.3.2.py
+++ 1/3/2/1.3.2.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+def frequences(string) :
+    chars = [char for char in string]
+    occurence = {}
+    for char in chars :
+        if occurence.get(char) :
+            occurence[char] +=  1
+        else :
+            occurence[char] = 1
+    print(occurence)
+
+frequences("Etre contesté, c’est être constaté")
