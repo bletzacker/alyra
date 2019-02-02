bb30f8e7a93905bb2b7b3f3fc1b0151fdac8be53
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-02 16:23:41 +0100

    Exercice 1.3.1 version 1

--- 1/3/1/1.3.1.py
+++ 1/3/1/1.3.1.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+def chiffreCesar(string, decalage) :
+    chars = [char for char in string]
+    return print(''.join(list(map(lambda char: chr(ord(char) + decalage), chars))))
+
+chiffreCesar("abc", 1)
