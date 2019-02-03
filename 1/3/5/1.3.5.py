be3a1381cf56897aac69a2a3995427c249795f41
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-03 09:17:06 +0100

    Exercice 1.3.5 version 1

--- 1/3/5/1.3.5.py
+++ 1/3/5/1.3.5.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import random
+import hashlib
+import base58
+
+def sha256(bstring) :
+    return hashlib.sha256(str.encode(bstring)).hexdigest()
+
+def ripemd160(bstring) :
+    hash = hashlib.new('ripemd160')
+    hash.update((str.encode(bstring)))
+    return hash.hexdigest()
+
+class Bitcoin_address :
+    def __init__(self,key) :
+        hash = ripemd160(sha256(str(key)))
+        hash = '0x00' + hash
+        checksum = sha256(sha256(hash))
+        hash = hash + checksum[:8]
+        self.bitcoin_address = base58.b58encode(hash)
+
+    def __str__(self) :
+        return '{}'.format(self.bitcoin_address.decode("utf-8"))
+
+key = random.random()
+print(Bitcoin_address(key))
