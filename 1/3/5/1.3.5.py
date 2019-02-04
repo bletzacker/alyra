b964380837484642033a67632fd7c512bdf5fc55
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-04 22:41:17 +0100

    Exercice 1.3.5 version 2

--- 1/3/5/1.3.5.py
+++ 1/3/5/1.3.5.py
@@ -5,24 +5,24 @@ import random
 import hashlib
 import base58
 
-def sha256(bstring) :
-    return hashlib.sha256(str.encode(bstring)).hexdigest()
+def sha256(key) :
+    return hashlib.sha256(key).digest()
 
-def ripemd160(bstring) :
+def ripemd160(key) :
     hash = hashlib.new('ripemd160')
-    hash.update((str.encode(bstring)))
-    return hash.hexdigest()
+    hash.update(key)
+    return hash.digest()
 
 class Bitcoin_address :
     def __init__(self,key) :
-        hash = ripemd160(sha256(str(key)))
-        hash = '0x00' + hash
-        checksum = sha256(sha256(hash))
-        hash = hash + checksum[:8]
+        hash = b'\x00' + ripemd160(sha256(key))
+        checksum = sha256(sha256(hash)).hex()
+        hash = hash.hex()
+        hash += checksum[:8]
         self.bitcoin_address = base58.b58encode(hash)
 
     def __str__(self) :
         return '{}'.format(self.bitcoin_address.decode("utf-8"))
 
-key = random.random()
+key = random.getrandbits(256).to_bytes(32, 'big')
 print(Bitcoin_address(key))
