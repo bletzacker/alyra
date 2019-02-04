e8815825a11bd53317315b9d4937adeca0a3dfd9
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-04 12:21:33 +0100

    Exercice 1.4.6 version 1

--- 1/4/6/1.4.6.py
+++ 1/4/6/1.4.6.py
@@ -1,2 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def pile_vide() :
+    return []
+
+def empiler(data,p) :
+    p.append(data)
+    return p
+
+def depiler(p) :
+    return p.pop()
+
+def sommet(p) :
+    return p[-1]
+
+def calculatrice_NPI(expression) :
+
+    chars = expression.split(" ")
+    p = pile_vide()
+
+    for char in chars :
+        if char.isdigit() :
+            empiler(float(char),p)
+        else :
+            resultat = {
+            '+' : lambda x,y : y + x,
+            '-' : lambda x,y : y - x,
+            '*' : lambda x,y : y * x,
+            '/' : lambda x,y : y / x,
+            '=' : lambda x,y : y == x,
+            '<>' : lambda x,y : y != x
+            }[char](depiler(p),depiler(p))
+            empiler(resultat,p)
+
+    return sommet(p)
+
+expression ="21 7 / 4 2 / - 1 =" # True
+print("L'évaluation de + " + expression + " est :", calculatrice_NPI(expression))
+
+expression ="21 7 / 4 2 / - 1 <>" # False
+print("L'évaluation de + " + expression + " est :", calculatrice_NPI(expression))
