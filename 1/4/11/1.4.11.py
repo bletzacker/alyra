fdc1db9673f70578c019b7118ff3839f9b17c373
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-04 16:10:27 +0100

    Exercice 1.4.11 version 2

--- 1/4/11/1.4.11.py
+++ 1/4/11/1.4.11.py
@@ -17,6 +17,27 @@ def rechercheDebut(string,size) :
     return time.time() - t1
 
 for size in range(10,110,10) :
-    n = 100 # Calcul d'un temps moyen comme estimateur sans biais pour une même taille (sinon problématique des fluctuations aléatoires)
-    temps_moyen = statistics.mean([rechercheDebut("AA",size) for _ in range(n)])
-    print("Taille :",size,"Temps moyen (s) :", temps_moyen)
+    i = 100 # Calcul d'un temps moyen comme estimateur sans biais pour une même taille (sinon problématique des fluctuations aléatoires)
+    temps_moyen = statistics.mean([rechercheDebut("AA",size) for _ in range(i)])
+    print("Longueur Chaîne :",len("AA"),"Taille :",size,"Temps moyen (s) :", temps_moyen)
+
+size = 10
+
+i = 10 # Calcul d'un temps moyen comme estimateur sans biais pour une même taille (sinon problématique des fluctuations aléatoires)
+temps_moyen = statistics.mean([rechercheDebut("A",size) for _ in range(i)])
+print("Longueur Chaîne :",len("A"),"Taille :",size,"Temps moyen (s) :", temps_moyen)
+
+i = 10 # Calcul d'un temps moyen comme estimateur sans biais pour une même taille (sinon problématique des fluctuations aléatoires)
+temps_moyen = statistics.mean([rechercheDebut("AA",size) for _ in range(i)])
+print("Longueur Chaîne :",len("AA"),"Taille :",size,"Temps moyen (s) :", temps_moyen)
+
+i = 10 # Calcul d'un temps moyen comme estimateur sans biais pour une même taille (sinon problématique des fluctuations aléatoires)
+temps_moyen = statistics.mean([rechercheDebut("AAA",size) for _ in range(i)])
+print("Longueur Chaîne :",len("AAA"),"Taille :",size,"Temps moyen (s) :", temps_moyen)
+
+i = 10 # Calcul d'un temps moyen comme estimateur sans biais pour une même taille (sinon problématique des fluctuations aléatoires)
+temps_moyen = statistics.mean([rechercheDebut("AAAA",size) for _ in range(i)])
+print("Longueur Chaîne :",len("AAAA"),"Taille :",size,"Temps moyen (s) :", temps_moyen)
+
+# Temps approximatif pour obtenir les résultats : 2 mn
+# Conclusion : La complexité est nettement supérieure pour la longueur du motif à chercher par rapport à la taille n.
