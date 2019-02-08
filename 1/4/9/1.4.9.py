85127308a3ad1839330af88e44504badabe4de46
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-08 10:21:56 +0100

    Exercice 1.4.9 version 2

--- 1/4/9/1.4.9.py
+++ 1/4/9/1.4.9.py
@@ -11,5 +11,4 @@ now = time.strftime('%Y-%m-%d %H:%M:%S UTC', time.gmtime())
 height_latest_block = blockexplorer.get_latest_block().height
 print("Nous pouvons calculer précisément la taille de l’ensemble des en-têtes.")
 print("Nous multiplions la taille d'une en-tête (80 octets) par le nombre de blocs à ce jour : ",height_latest_block)
-print("Taille de l’ensemble des en-têtes le ", now, ": ", 80 * height_latest_block / 1e6, "Mo")
-# 2019-02-04 06:30:05 UTC :  44.91696 Mo
+print("Taille de l’ensemble des en-têtes le ", now, ": ", 80 * (height_latest_block + 1) / 1e6, "Mo")
