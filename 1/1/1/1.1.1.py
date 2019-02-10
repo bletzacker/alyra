1be53e06ea6dbb00704488d4359d39214b966942
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-10 22:22:09 +0100

    Exercice 1.1.1 pylint

--- 1/1/1/1.1.1.py
+++ 1/1/1/1.1.1.py
@@ -1,37 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+    Exercice 1.1.1
+"""
+
 import random
 
-target = random.randint(1, 100)
-notFound = True
+def game():
 
-print("J'ai tiré aléatoirement un nombre entre 1 et 100 que je vous propose de retrouver.\n")
+    """
+        function
+    """
 
-while notFound:
+    target = random.randint(1, 100)
+    not_found = True
 
-    notIntegerBetweenOneAndHundred = True
-    while notIntegerBetweenOneAndHundred :
-        proposal = input("Entrez votre proposition sous la forme d'un entier entre 1 et 100 : ")
-        if proposal.isdigit() :
-            proposal = int(proposal)
-            if 1 <= proposal <= 100 :
-                notIntegerBetweenOneAndHundred = False
+    print("J'ai tiré aléatoirement un nombre entre 1 et 100 que je vous propose de retrouver.\n")
 
-    difference = proposal - target
+    while not_found:
 
-    if difference == 0 :
-        print("Bravo ! Vous avez trouvé le nombre que j'ai tiré aléatoirement.\n")
-        notFound = False
-    else :
-        if difference > 0 :
-            print("Mon nombre est inférieur.")
-        else :
-            print("Mon nombre est supérieur.")
+        not_integer_between_one_and_hundred = True
+        while not_integer_between_one_and_hundred:
+            proposal = input("Entrez votre proposition sous la forme d'un entier entre 1 et 100 : ")
+            if proposal.isdigit():
+                proposal = int(proposal)
+                if 1 <= proposal <= 100:
+                    not_integer_between_one_and_hundred = False
 
-        if abs(difference) <= 5 :
-            print("De plus, votre proposition est très proche, moins de 5 unités d'écart.\n")
-        elif abs(difference) <= 10 :
-            print("De plus, Votre proposition est proche, entre 6 et 10 unités d'écart.\n")
-        else :
-            print("De plus, votre proposition est éloignée de plus de 10 unités.\n")
+        difference = proposal - target
+
+        if difference == 0:
+            print("Bravo ! Vous avez trouvé le nombre que j'ai tiré aléatoirement.\n")
+            not_found = False
+        else:
+            if difference > 0:
+                print("Mon nombre est inférieur.")
+            else:
+                print("Mon nombre est supérieur.")
+
+            if abs(difference) <= 5:
+                print("De plus, votre proposition est très proche, moins de 5 unités d'écart.\n")
+            elif abs(difference) <= 10:
+                print("De plus, Votre proposition est proche, entre 6 et 10 unités d'écart.\n")
+            else:
+                print("De plus, votre proposition est éloignée de plus de 10 unités.\n")
+
+game()
