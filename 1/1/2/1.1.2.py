8f5f1dcd98eb4213c0aec6d8d2c803d11d61728a
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-11 09:01:43 +0100

    Exercice 1.1.2 pylint

--- 1/1/2/1.1.2.py
+++ 1/1/2/1.1.2.py
@@ -1,39 +1,60 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Utilisation d'un algorithme de recherche par dichotomie itératif
+"""
+    Exercice 1.1.1
+    Utilisation d'un algorithme de recherche par dichotomie itératif
+"""
 
-def saisie_utilisateur() :
-    notIntegerBetweenOneAndHundred = True
-    while notIntegerBetweenOneAndHundred :
+def saisie_utilisateur(borne_min, borne_max):
+
+    """
+        function
+    """
+
+    not_integer_between_one_and_hundred = True
+    while not_integer_between_one_and_hundred:
         solution = input("Saisir un nombre entre 1 et 100 : ")
-        if solution.isdigit() :
+        if solution.isdigit():
             solution = int(solution)
-            if min <= solution <= max :
-                notIntegerBetweenOneAndHundred = False
+            if borne_min <= solution <= borne_max:
+                not_integer_between_one_and_hundred = False
                 return solution
 
-def verifie(estimation,solution) :
-    if estimation == solution :
-        return True
-    else :
-        return False
+def verifie(estimation, solution):
 
-def recherche(solution) :
-    a = min
-    b = max
+    """
+        function
+    """
+
+    return bool(estimation == solution)
+
+def recherche(solution, borne_min, borne_max):
+
+    """
+        function
+    """
+
     estimation = 0
     i = 0
-    while not verifie(estimation,solution):
-        estimation = int((a+b)/2)
-        if solution > estimation :
-            a = estimation + 1
-        else :
-            b = estimation - 1
-        i+=1
+    while not verifie(estimation, solution):
+        estimation = int((borne_min+borne_max)/2)
+        if solution > estimation:
+            borne_min = estimation + 1
+        else:
+            borne_max = estimation - 1
+        i += 1
     print("\nJ'ai trouvé en " + str(i) + " itérations. Le nombre saisi est : " + str(estimation))
 
-min = 1
-max = 100
-solution = saisie_utilisateur()
-recherche(solution)
+def dichotomie():
+
+    """
+        function
+    """
+
+    borne_min = 1
+    borne_max = 100
+    solution = saisie_utilisateur(borne_min, borne_max)
+    recherche(solution, borne_min, borne_max)
+
+dichotomie()
