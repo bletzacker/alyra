624b46cb57125cdb41c53872cc97ce832788d4f9
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-11 09:38:15 +0100

    Exercice 1.1.4 pylint

--- 1/1/4/1.1.4.py
+++ 1/1/4/1.1.4.py
@@ -1,19 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+    Exercice 1.1.4
+"""
+
 import math
 
-class Cercle :
+class Cercle:
 
-    def __init__(self, rayon) :
-      self.rayon = rayon
+    """
+        class
+    """
 
-    def aire(self) :
+    def __init__(self, rayon):
+        self.rayon = rayon
+
+    def aire(self):
+
+        """
+            function
+        """
+
         return math.pi*self.rayon**2
 
-    def perimetre(self) :
+    def perimetre(self):
+
+        """
+            function
+        """
+
         return 2*math.pi*self.rayon
 
-cercle = Cercle(1)
-print("L'aire du disque de rayon " + str(cercle.rayon) + " est " + str(cercle.aire()))
-print("Le périmètre du cercle de rayon " + str(cercle.rayon) + " est " + str(cercle.perimetre()))
+def exemple():
+
+    """
+        function
+    """
+
+    cercle = Cercle(1)
+    print("L'aire du disque de rayon " + str(cercle.rayon) + " est " \
+    + str(cercle.aire()))
+    print("Le périmètre du cercle de rayon " + str(cercle.rayon) + " est " \
+    + str(cercle.perimetre()))
+
+exemple()
