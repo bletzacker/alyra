dc24d35eca4495e378f84abf4ce33a691357bc0e
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-12 12:02:52 +0100

    Exercice 1.4.14 version 1

--- 1/4/14/1.4.14.py
+++ 1/4/14/1.4.14.py
@@ -1,2 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+"""
+Exercice 1.4.14
+"""
+
+def bits2target(bits):
+    """
+    function bits2target(bits)
+    """
+    bits = bytes.fromhex(bits)
+    return bits[:1], bits[1:]
+
+def cible_atteinte(coefficient, exposant, hexstr_hash):
+    """
+    cible_atteinte(coefficient, exposant, hexstr_hash)
+    """
+    hash_to_test = bytes.fromhex(hexstr_hash)
+    target = coefficient + int.from_bytes(exposant, 'big') * b'\x00'
+    return int.from_bytes(hash_to_test, 'big') < int.from_bytes(target, 'big')
+
+def test():
+    """
+    test()
+    """
+    hexstr_hash = "0000000000ffffffffffffffffffffffffffffffffffffffffffffffffffff"
+    exposant, coefficient = bits2target("173218a5")
+    print(cible_atteinte(coefficient, exposant, hexstr_hash))
+
+test()
