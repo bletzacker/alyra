c969bcf3a86e61d4233b159d28477e021d05f227
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-16 18:11:40 +0100

    Exercice 2.1.1 version 1

--- 2/1/1/2.1.1.py
+++ 2/1/1/2.1.1.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+"""
+    Exercice 2.1.1
+"""
+
+def calculer_difficulte(target):
+    """
+        calculer_difficulte(target)
+    """
+    targetmax = 0x00000000FFFF0000000000000000000000000000000000000000000000000000
+    return targetmax / target
+
+print(calculer_difficulte(1147152896345386682952518188670047452875537662186691235300769792000))
