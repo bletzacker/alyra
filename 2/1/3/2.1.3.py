283998c157737c6ded6c12cc4f8a087dee839df6
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-21 16:07:18 +0100

    Exercice 2.1.3 version 1

--- 2/1/3/2.1.3.py
+++ 2/1/3/2.1.3.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+    Exercice 2.1.3
+"""
+
+def bloc_reajustement(height) :
+    """
+        function bloc_reajustement(height)
+    """
+    return not(height % 2016)
+
+print(bloc_reajustement(556416))
