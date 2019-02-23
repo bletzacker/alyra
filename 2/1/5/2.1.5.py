0cc20f076032ee1cadff5e8c65e226ffbbd45459
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-23 08:37:09 +0100

    Exercice 2.1.5 version 1

--- 2/1/5/2.1.5.py
+++ 2/1/5/2.1.5.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+    Exercice 2.1.5
+"""
+
+def bloc_reward(height) :
+    """
+        function bloc_reward(height)
+    """
+    return 50 / (2 ** (height // 210000))
+
+print(bloc_reward(564282))
