eec0080674cd5479e98e753f2484abac05e82e7b
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-23 11:48:45 +0100

    Exercice 2.1.8 version 1

--- 2/1/8/2.1.8.py
+++ 2/1/8/2.1.8.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# Dans quel état est chaque dépôt ? (git status)
+
+# Le dépot ALPHA indique que les changements du fichier <Prénom>.secret
+# n'est pas suivi conformément à n'avoir "commit" que le premier
+# fichier <Prénom>.md.
+# Le dépot BETA indique qu'il n'y à rien à "commit".
+
+# Quel dépôt le plus à jour de tous les changements ?
+
+# Les deux dépôts me semble à jour après la résolution du conflit si nous
+# choissisons de mettre aaaaaa dans <Prénom>.md de BETA.
+
+# Cependant, il me semble que c'est le dépôt BETA qui serait le plus à jour
+# si un autre choix de résolution du conflit était effectué.
+
+# Serait-il possible d'avoir une explication complémentaire pour l'analogie
+# avec la blockchain ?
