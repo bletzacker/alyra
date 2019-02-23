4f4b86aedad99d81f5765d6302f8e34b8c7993b4
Laurent BLETZACKER <laurent@bletzacker.io>
2019-02-23 09:34:02 +0100

    Exercice 2.1.6 version 1

--- 2/1/6/2.1.6.py
+++ 2/1/6/2.1.6.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+    Exercice 2.1.6
+"""
+
+def total_bitcoin(height) :
+    """
+        function total_bitcoin(height)
+    """
+    total_bitcoin = 0
+    for i in range(height // 210000 + 1) :
+        total_bitcoin += min(210000,height+1) * (50 / (2 ** (i)))
+        height -= 210000
+    return total_bitcoin
+
+print(total_bitcoin(564288))
