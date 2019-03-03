fa1986ee9ef176282fb579abc2171c44500885bb
Laurent BLETZACKER <laurent@bletzacker.io>
2019-03-03 11:45:43 +0100

    Exercice 3.1.2.2 version 1

---
+++ 3/1/2/2/3.1.2.2.sol
@@ -0,0 +1,43 @@
+pragma solidity ^0.4.25;
+
+contract Assemblee{
+    address[] membres;
+
+    string[] public descriptionsDecisions;
+    uint[] public votePour;
+    uint[] public voteContre;
+
+    function rejoindre() public {
+        membres.push(msg.sender);
+    }
+
+    function estMembre(address utilisateur) public view returns (bool) {
+        for (uint i=0; i<membres.length; i++) {
+            if (membres[i] == utilisateur) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    function proposerDecision(string decision) public {
+      if (estMembre(msg.sender)) {
+        descriptionsDecisions.push(decision);
+        votePour.push(0);
+        voteContre.push(0);
+      }
+    }
+
+    function vote(uint proposition, uint vote) public returns (bool) {
+      if (vote == 1) {
+          votePour[proposition] += 1;
+          return true;
+      } else if (vote == 0) {
+          voteContre[proposition] += 1;
+          return true;
+      } else {
+          return false;
+      }
+    }
+}
